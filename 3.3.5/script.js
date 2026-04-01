9b5e2249183543e48e10636f74f1b44ce52a9c9f
10INF02 <juli.cerman@email.cz>
2026-04-01 11:16:44 +0200

    update

--- 3.3.5/script.js
+++ 3.3.5/script.js
@@ -21,7 +21,7 @@ console.log(message);
 
 // Druhé řešení pomocí ternárního operátoru
 
-let number2 = 10
+let number2 = 10;
 let message2 = (number2 > 0) ?"Kladné číslo" : "Není kladné";
 
 console.log(message2)
